24d697184903eccfd663759ab1445cfb62310d65
lenyn15 <smithdash2570@gmail.com>
2021-11-20 14:58:14 -0500

    enrolment service created

---
+++ enrolment-service/src/main/java/lenyn/code/enrolmentservice/EnrolmentServiceApplication.java
@@ -0,0 +1,13 @@
+package lenyn.code.enrolmentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnrolmentServiceApplication {
+
+    public static void main( String[] args ) {
+        SpringApplication.run( EnrolmentServiceApplication.class, args );
+    }
+
+}
